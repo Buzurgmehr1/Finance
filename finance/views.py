16a207ea3b0dee40be9e3d19d05c6b6978fb8b83
Buzurgmehr1 <bsftraders4@gmail.com>
2025-07-20 06:39:20 -0700

    All done in class

--- finance/views.py
+++ finance/views.py
@@ -79,6 +79,7 @@ def category_create_view(request):
     )
     return redirect('home')
 
+
 def income_create_view(request):
     user = get_current_user_view(request)
     if not user:
@@ -97,6 +98,24 @@ def income_create_view(request):
             user = user
         )
         return redirect('home')
+    
+def income_edit_view(request, pk):
+    user = get_current_user_view(request)
+    if not user:
+        return redirect('login')
+
+    income = Income.objects.filter(id=pk, user=user).first()
+    if not income:
+        return HttpResponse("Income not found or access denied")
+
+    if request.method == 'POST':
+        income.amount = request.POST.get('amount',False)
+        income.category = request.POST.get('category',False)
+        income.description = request.POST.get('description',False)
+        income.save()
+        return redirect('home')
+
+    return render(request, 'income_edit.html', {'income': income})
 
 def expense_create_view(request):
     user = get_current_user_view(request)
@@ -116,4 +135,22 @@ def expense_create_view(request):
             user = user
         )
         return redirect('home')
+
+def expense_edit_view(request, pk):
+    user = get_current_user_view(request)
+    if not user:
+        return redirect('login')
+
+    expense = Expense.objects.filter(id=pk, user=user).first()
+    if not expense:
+        return HttpResponse("Expense not found or access denied")
+
+    if request.method == 'POST':
+        expense.amount = request.POST.get('amount',False)
+        expense.category = request.POST.get('category',False)
+        expense.description = request.POST.get('description',False)
+        expense.save()
+        return redirect('home')
+
+    return render(request, 'expense_edit.html', {'expense': expense})
 
